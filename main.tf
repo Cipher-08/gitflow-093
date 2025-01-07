ce15c35c0bcbb79a028e306fb29a605502911f9c
Cipher-08 <sakshamawasthi1111@gmail.com>
2025-01-07 17:04:45 +0530

    Add analytics feature with S3 bucket

--- main.tf
+++ main.tf
@@ -8,3 +8,9 @@ resource "aws_vpc" "infra_vpc" {
     Name = "Infrasity-vpc"
   }
 }
+
+resource "aws_s3_bucket" "analytics_bucket" {
+  bucket = "analytics-feature-bucket"
+  acl    = "private"
+}
+
